a18d71ccd953d7fd888d44bbe96b76c3cd0ea3c9
charles <charles76680119@gmail.com>
2024-01-25 08:10:38 +0800

    Add test for fileutil named TestCheckFileExist

---
+++ fileutil/fileutil_test.go
@@ -0,0 +1,40 @@
+package fileutil
+
+import (
+	"os"
+	"testing"
+)
+
+func TestCheckFileExist(t *testing.T) {
+	existFile := "existFile.txt"
+	nonExistFile := "nonExistFile.txt"
+	tempDir := t.TempDir()
+
+	// create the existFile and keep nonExistFile non exist
+	file, err := os.Create(tempDir + "/" + existFile)
+	if err != nil {
+		t.Error(err)
+	}
+	defer closeWithHandleError(file)
+
+	// check
+	if !CheckFileExist(tempDir + "/" + existFile) {
+		t.Error(existFile + " File should exist")
+	}
+
+	if CheckFileExist(tempDir + "/" + nonExistFile) {
+		t.Error(nonExistFile + " File should not exist")
+	}
+
+	// now create the nonExistFile and check again
+	fileNonExist, err := os.Create(tempDir + "/" + nonExistFile)
+	if err != nil {
+		t.Error(err)
+	}
+	defer closeWithHandleError(fileNonExist)
+
+	// check nonExistFile is exist
+	if !CheckFileExist(tempDir + "/" + nonExistFile) {
+		t.Error("in second test , " + nonExistFile + " File should exist")
+	}
+}
